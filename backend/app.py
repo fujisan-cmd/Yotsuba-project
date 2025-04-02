2392abe244951325a9258ea35b0e28c6d35a5166
fujisan <fujiemasahiro88@gmail.com>
2025-04-02 21:26:57 +0900

    neo4jとの連携をテスト実装

--- backend/app.py
+++ backend/app.py
@@ -3,6 +3,16 @@ from fastapi.middleware.cors import CORSMiddleware
 from pydantic import BaseModel
 import requests
 import json
+import os
+from dotenv import load_dotenv
+from neo4j import GraphDatabase
+
+load_dotenv()
+
+uri = os.getenv("NEO4J_URI")
+user = os.getenv("NEO4J_USER")
+password = os.getenv("NEO4J_PASSWORD")
+driver = GraphDatabase.driver(uri, auth=(user, password))
 
 app = FastAPI()
 
@@ -15,6 +25,43 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
+def clear_db(tx):
+    tx.run('MATCH (n) OPTIONAL MATCH (n)-[r]-() DELETE n,r')
+
+def add_person_node(tx, name):
+    tx.run('CREATE (p:Person {name: $name}) RETURN p', {'name': name})
+
+def add_friend_relationship(tx, name, friend_name=None):
+    if not friend_name:
+        tx.run('CREATE (p:Person {name: $name}) RETURN p', {'name': name})
+    else:
+        tx.run('MATCH (p:Person {name: $name}) '
+               'CREATE (p)-[:FRIEND]->(:Person {name: $friend_name})',
+               name=name, friend_name=friend_name)
+        
+def search_all(tx):
+    result = tx.run('MATCH (n) OPTIONAL MATCH (n)-[r]-() RETURN n,r')
+    return [r for r in result]
+
+def node_to_dict(node):
+    """Neo4jのNodeオブジェクトを辞書に変換"""
+    if isinstance(node, dict):  # すでに辞書ならそのまま
+        return node
+    elif hasattr(node, "items"):  # Nodeオブジェクトの場合
+        return dict(node.items())
+    return node  # そのまま返す
+
 @app.get("/")
 def index():
     return {"message": "Pong!"}
+
+@app.get("/graph_info")
+def get_graph_info():
+    with driver.session() as session:
+        session.execute_write(clear_db)
+        session.execute_write(add_person_node, 'Taro')
+        session.execute_write(add_person_node, 'Hanako')
+        session.execute_write(add_friend_relationship, 'Taro', 'Hanako')
+        result = session.execute_read(search_all)
+        json_str = json.dumps(result, default=node_to_dict, indent=2)
+    return json_str
--- frontend/src/app/top/page.jsx
+++ frontend/src/app/top/page.jsx
@@ -1,12 +1,18 @@
 "use client";
 import { useState } from "react";
 import fetchtest from "./fetchtest";
+import neo4j_fetchtest from "./neo4j_fetchtest";
 
 export default function Page(){
     const [APIresult, setAPIresult] = useState(["Ping..."]);
+    const [Neo4Jresult, setNeo4Jresult] = useState([""]);
     const APItest = async () => {
         const pong = await fetchtest();
         setAPIresult(pong.message);
+    };
+    const Neo4Jtest = async () => {
+        const res = await neo4j_fetchtest();
+        setNeo4Jresult(res);
     };
 
     return(
@@ -16,6 +22,11 @@ export default function Page(){
             Ping
         </button>
         <p>{APIresult}</p>
+
+        <button onClick={Neo4Jtest} className="btn btn-neutral w-full border-0 bg-blue-200 text-black hover:text-white">
+            Get Graph Data
+        </button>
+        <p>{Neo4Jresult}</p>
         </>
     );
 }
