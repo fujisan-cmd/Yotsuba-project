dc473813ff9b81fd1eef8e357766d84df8e50653
kimura <clookout@gmail.com>
2025-04-16 06:58:54 +0900

    最終調整版-0659

--- frontend/src/app/mymap/old/neo4j_fetch.js
+++
@@ -1,8 +0,0 @@
-export default async function neo4j_fetch(token){
-    const res = await fetch(process.env.NEXT_PUBLIC_API_ENDPOINT+'/graph_info',
-        {
-            headers: {Authorization: `Bearer ${token}`,},
-        } 
-    );
-    return res.json();
-}
